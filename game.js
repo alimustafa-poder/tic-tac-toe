49af2d03b1c80b7b26ddfeedf28ff8bc347710f4
Ali Mustafa <24090078@lums.edu.pk>
2022-07-16 19:01:47 +0500

    Fixed formatting

--- game.js
+++ game.js
@@ -42,15 +42,15 @@ const gameBoard = (() => {
             elem.addEventListener('click', () => {
                 let elemIndex = elem.getAttribute('data-Array').split(',');
                 if (elem.innerHTML != "") return;
-                if (turn % 2 == 1) {   
+                if (turn % 2 == 1) {
                     playerTurn.innerHTML = '';
-                    playerTurn.innerHTML = `<h2 class="turn">Player ${(player2.sign).toUpperCase()}'s turn.</h2>`;              
+                    playerTurn.innerHTML = `<h2 class="turn">Player ${(player2.sign).toUpperCase()}'s turn.</h2>`;
                     gameArr[elemIndex[0]][elemIndex[1]] = player1.sign;
                     elem.innerHTML += '<i class="fa-solid fa-check fa-2x"></i>';
                     turn++;
                 } else {
                     playerTurn.innerHTML = '';
-                    playerTurn.innerHTML = `<h2 class="turn">Player ${(player1.sign).toUpperCase()}'s turn.</h2>`;  
+                    playerTurn.innerHTML = `<h2 class="turn">Player ${(player1.sign).toUpperCase()}'s turn.</h2>`;
                     gameArr[elemIndex[0]][elemIndex[1]] = player2.sign;
                     elem.innerHTML += '<i class="fa-regular fa-circle fa-2x"></i>';
                     turn++;
@@ -68,81 +68,73 @@ const gameBoard = (() => {
             winner.innerHTML = `<p>Player ${(gameArr[0][0]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else if (gameArr[1][0] == gameArr[1][1] &&
+        } else if (gameArr[1][0] == gameArr[1][1] &&
             gameArr[1][1] == gameArr[1][2] &&
             gameArr[1][0] != '' && gameArr[1][1] != '' && gameArr[1][2] != '') {
             winner.innerHTML = '';
             winner.innerHTML = `<p>Player ${(gameArr[1][0]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else if (gameArr[2][0] == gameArr[2][1] &&
+        } else if (gameArr[2][0] == gameArr[2][1] &&
             gameArr[2][1] == gameArr[2][2] &&
             gameArr[2][0] != '' && gameArr[2][1] != '' && gameArr[2][2] != '') {
             winner.innerHTML = '';
             winner.innerHTML = `<p>Player ${(gameArr[2][0]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else if (gameArr[0][0] == gameArr[1][0] &&
+        } else if (gameArr[0][0] == gameArr[1][0] &&
             gameArr[1][0] == gameArr[2][0] &&
             gameArr[0][0] != '' && gameArr[1][0] != '' && gameArr[2][0] != '') {
             winner.innerHTML = '';
             winner.innerHTML = `<p>Player ${(gameArr[0][0]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else if (gameArr[0][1] == gameArr[1][1] &&
+        } else if (gameArr[0][1] == gameArr[1][1] &&
             gameArr[1][1] == gameArr[2][1] &&
             gameArr[0][1] != '' && gameArr[1][1] != '' && gameArr[2][1] != '') {
             winner.innerHTML = '';
             winner.innerHTML = `<p>Player ${(gameArr[0][1]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else if (gameArr[0][2] == gameArr[1][2] &&
+        } else if (gameArr[0][2] == gameArr[1][2] &&
             gameArr[1][2] == gameArr[2][2] &&
             gameArr[0][2] != '' && gameArr[1][2] != '' && gameArr[2][2] != '') {
             winner.innerHTML = '';
             winner.innerHTML = `<p>Player ${(gameArr[0][2]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else if (gameArr[0][2] == gameArr[1][1] &&
+        } else if (gameArr[0][2] == gameArr[1][1] &&
             gameArr[1][1] == gameArr[2][0] &&
             gameArr[0][2] != '' && gameArr[1][1] != '' && gameArr[2][0] != '') {
             winner.innerHTML = '';
             winner.innerHTML = `<p>Player ${(gameArr[0][2]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else if (gameArr[0][0] == gameArr[1][1] &&
+        } else if (gameArr[0][0] == gameArr[1][1] &&
             gameArr[1][1] == gameArr[2][2] &&
             gameArr[0][0] != '' && gameArr[1][1] != '' && gameArr[2][2] != '') {
             winner.innerHTML = '';
             winner.innerHTML = `<p>Player ${(gameArr[0][0]).toUpperCase()} has won.</p>`;
             gameReset.classList.add('active');
             gameDiv.style.pointerEvents = 'none';
-        }
-        else{
+        } else {
             if (turn == 10) {
-            winner.innerHTML = '';
-            winner.innerHTML = `<p>The game is a tie.</p>`;
-            gameReset.classList.add('active');
-            gameDiv.style.pointerEvents = 'none';
+                winner.innerHTML = '';
+                winner.innerHTML = `<p>The game is a tie.</p>`;
+                gameReset.classList.add('active');
+                gameDiv.style.pointerEvents = 'none';
             }
         }
     }
 
-    let resetGame = () =>{
+    let resetGame = () => {
         turn = 1;
         gameArr = [
             ['', '', ''],
             ['', '', ''],
             ['', '', '']
         ];
-        paraElem.forEach((elem)=>{
+        paraElem.forEach((elem) => {
             elem.textContent = '';
         })
         game();
@@ -158,4 +150,4 @@ const gameBoard = (() => {
 gameBoard.append(gameArr);
 let paraElem = document.querySelectorAll('.gameBoard>p');
 gameBoard.game();
-resetButton.addEventListener('click', ()=>gameBoard.resetGame());
+resetButton.addEventListener('click', () => gameBoard.resetGame());
